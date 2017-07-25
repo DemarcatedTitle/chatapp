51d677bc0b415ebc06f70f6c2d7a83200de0d834
dt <dimmton@gmail.com>
2017-07-24 22:38:28 -0700

    Starting to get auth set up

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,9 @@
 const Hapi = require("hapi");
 const Inert = require("inert");
 const path = require("path");
+const JWT = require("jsonwebtoken");
+
+var users = new Map();
 
 var server = new Hapi.Server({
     connections: {
@@ -13,32 +16,88 @@ var server = new Hapi.Server({
     }
 });
 
-server.register(Inert, () => {});
-server.connection({ port: 4000 });
-
-server.route({
-    method: "GET",
-    path: "/",
-    handler: function(request, reply) {
-        reply.file("index.html");
+var people = {
+    1: {
+        id: 1,
+        name: "Anthony Valid User"
     }
-});
+};
 
-var io = require("socket.io")(server.listener);
+var token = JWT.sign(people[1], "nevershareyoursecret");
+console.log(token);
 
-var chatlogs = [];
+var validate = function(decoded, request, callback) {
+    console.log("validate");
+    if (!people[decoded.id]) {
+        console.log("null, false");
+        return callback(null, false);
+    } else {
+        console.log("null, true");
+        return callback(null, true);
+    }
+};
 
-io.on("connection", function(socket) {
-    io.emit("chat message", chatlogs);
-    console.log("A user has connected");
-    socket.on("chat message", function(msg) {
-        chatlogs.push({
-            date: new Date(),
-            message: msg,
-            username: "longer user name let's see how long"
-        });
-        io.emit("chat message", chatlogs);
-        console.log("Message received");
+server.register(Inert, () => {});
+server.connection({ port: 8000, labels: "login" });
+const login = server.select("login");
+login.register(require("hapi-auth-jwt2"), function(err) {
+    if (err) {
+        console.log(err);
+    }
+    login.auth.strategy("jwt", "jwt", {
+        key: "NeverShareYourSecret",
+        validateFunc: validate,
+        verifyOptions: { ignoreExpiration: true }
     });
+    login.auth.default("jwt");
+    login.route([
+        {
+            method: "GET",
+            path: "/",
+            config: { auth: false },
+            handler: function(request, reply) {
+                reply({ text: "Token not required" });
+            }
+        },
+        {
+            method: "GET",
+            path: "/restricted",
+            config: { auth: "jwt" },
+            handler: function(request, reply) {
+                reply({ message: "You used a valid token" }).header(
+                    "Authorization",
+                    request.headers.authorization
+                );
+            }
+        }
+    ]);
 });
+// server.connection({ port: 4000, labels: "socket.io" });
+
+// const socketio = server.select("socket.io");
+// socketio.route({
+//     method: "GET",
+//     path: "/",
+//     handler: function(request, reply) {
+//         reply.file("index.html");
+//     }
+// });
+
+// var io = require("socket.io")(server.listener);
+
+// var chatlogs = [];
+
+// io.on("connection", function(socket) {
+//     io.emit("chat message", chatlogs);
+//     console.log("A user has connected");
+//     socket.on("chat message", function(msg) {
+//         chatlogs.push({
+//             date: new Date(),
+//             message: msg,
+//             username: "longer user name let's see how long"
+//         });
+//         io.emit("chat message", chatlogs);
+//         console.log("Message received");
+//     });
+// });
 server.start();
--- back-end/registerAcct.js
+++ back-end/registerAcct.js
@@ -1,1 +1,11 @@
 const bcrypt = require("bcrypt");
+module.exports = {
+    hashPass: function(plainTextPass, callback) {
+        bcrypt.hash(plainTextPass, 10, function(err, hash) {
+            if (callback) {
+                callback(hash);
+            }
+            return hash;
+        });
+    }
+};
